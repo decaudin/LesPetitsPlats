b55d6cba3c8bf2f3e917d88ad179df9f34b99bc1
apprenti_développeur <xd.decaudin@gmail.com>
2024-06-12 21:37:09 +0200

    ajout/retrait commentaires

--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -20,6 +20,8 @@ export const handleDropdown = (titles, contents, vectors) => {
         });
     });
 
+    // Empêche le dropdown ouvert de se refermer au clic
+
     contents.forEach((content) => {   
         content.addEventListener('click', (e) => {
             e.stopPropagation();
@@ -35,49 +37,6 @@ export const handleDropdown = (titles, contents, vectors) => {
         });
     });
 }
-
-// Fonction de création d'un élément de la liste
-
-/*const createListItems = (items, listElement) => {
-
-    listElement.innerHTML = '';
-
-    items.forEach(item => {
-        const li = document.createElement('li');
-        li.classList.add('dropdown_list__item');
-        li.setAttribute("tabindex", 0);
-        li.textContent = item;
-        listElement.appendChild(li);
-    });
-};
-
-export const fillDropdowns = (recipes) => {
-
-    const ingredientsSet = new Set();
-    const appliancesSet = new Set();
-    const ustensilsSet = new Set();
-
-    recipes.forEach(recipe => {
-        recipe.ingredients.forEach(ingredient => {
-            ingredientsSet.add(ingredient.ingredient);
-        });
-        appliancesSet.add(recipe.appliance);
-        recipe.ustensils.forEach(ustensil => {
-            ustensilsSet.add(ustensil);
-        });
-    });
-
-    const ingredientsList = document.getElementById('ingredients_list');
-    const appliancesList = document.getElementById('appliances_list');
-    const ustensilsList = document.getElementById('ustensils_list');
-
-    // Appel de la fonction createListItems pour créer les 3 listes
-
-    createListItems(Array.from(ingredientsSet), ingredientsList);
-    createListItems(Array.from(appliancesSet), appliancesList);
-    createListItems(Array.from(ustensilsSet), ustensilsList);
-
-}; */
 
 
 
